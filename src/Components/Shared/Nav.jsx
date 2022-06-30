4ce982d3bf80838c01d74979473dc838c478bd01
Debashish D. Dev <hello@debashish.dev>
2022-07-01 00:29:02 +0600

    Added All three components

--- src/Components/Shared/Nav.jsx
+++ src/Components/Shared/Nav.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="navbar bg-base-100 border-b-2">
+    <div className="navbar bg-base-100 border-b-[1px]">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">FanTasktic Manager</a>
+        <Link to={'/'} className="btn btn-ghost normal-case text-xl">FanTasktic Manager</Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
---
+++ src/Pages/Done.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Done = () => {
+  return (
+    <div>
+      <h2 className="text-center text-3xl font-bold">Your Completed Task</h2>
+    </div>
+  );
+};
+
+export default Done;
