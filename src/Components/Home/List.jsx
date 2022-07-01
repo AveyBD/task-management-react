8962093acb299ace2593002d71f34b15cb69b750
Debashish D. Dev <hello@debashish.dev>
2022-07-01 17:15:55 +0600

    Updated UI

--- src/Components/Home/List.jsx
+++ src/Components/Home/List.jsx
@@ -30,25 +30,46 @@ const List = ({ item, refetch }) => {
       });
   };
   return (
-    <div className="flex items-center justify-between gap-1 border border-gray-100 p-1 mt-1">
-      <div className="flex items-center gap-2">
-        <input
-          type="checkbox"
-          onChange={() => complete(item._id)}
-          className="checkbox checkbox-xs"
-          checked={item.complete}
-        />
-        <h2 className="">{item.task}</h2>
+    <>
+      <input type="checkbox" id="editTask" class="modal-toggle" />
+      <div class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+          <h3 class="font-bold text-lg">Edit Your Task </h3>
+          <p class="py-4">
+            haven't found a suitable location to show the input field yet
+          </p>
+          <div class="modal-action">
+            <label for="editTask" class="btn">
+              Close
+            </label>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center gap-2">
-        <FontAwesomeIcon className="text-green-400" icon={faPen} />
-        <FontAwesomeIcon
-          onClick={() => handleDelete(item._id)}
-          className="text-red-400"
-          icon={faTrash}
-        />
+      <div className="flex items-center justify-between gap-1 border border-gray-100 p-1 mt-1">
+        <div className="flex items-center gap-2">
+          <input
+            type="checkbox"
+            onChange={() => complete(item._id)}
+            className="checkbox checkbox-xs"
+            checked={item.complete}
+          />
+          <h2 className="">{item.task}</h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <label htmlFor="editTask">
+            <FontAwesomeIcon
+              className="text-green-400 cursor-pointer"
+              icon={faPen}
+            />
+          </label>
+          <FontAwesomeIcon
+            onClick={() => handleDelete(item._id)}
+            className="text-red-400 cursor-pointer"
+            icon={faTrash}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
